5ca5e29b1612936d1473e322f9388a8ff0d41bf3
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-04-22 19:27:04 -0300

    buscarPorId

--- src/main/java/org/example/cineapi/controller/FilmeController.java
+++ src/main/java/org/example/cineapi/controller/FilmeController.java
@@ -4,13 +4,12 @@ import jakarta.validation.Valid;
 import org.example.cineapi.model.Filme;
 import org.example.cineapi.service.FilmeService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/filmes")
 public class FilmeController {
 
     private final FilmeService service;
@@ -22,6 +21,11 @@ public class FilmeController {
     @GetMapping
     public List<Filme> listar(){
         return service.listar();
+    }
+
+    @GetMapping("{/id}")
+    public Filme buscarPorId(@PathVariable Long idFilme){
+        return service.buscarPorId(idFilme);
     }
 
     @PostMapping
