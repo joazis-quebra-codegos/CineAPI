601bc51becedd89144fd7b22cbe89c597c490bf7
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-04-29 22:24:47 -0300

    Handler

--- src/main/java/org/example/cineapi/handler/GlobalExceptionHandler.java
+++ src/main/java/org/example/cineapi/handler/GlobalExceptionHandler.java
@@ -1,10 +1,23 @@
 package org.example.cineapi.handler;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler()
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> tratarValidação(MethodArgumentNotValidException ex){
+        Map<String, String> erros = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(
+                erro -> erros.put(erro.getField(), erro.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(erros);
+    }
 }
