6994143765eec60e94cd90b776b232c914d332d5
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2026-05-26 21:31:12 -0300

    pom e alterações passadas

--- src/main/java/org/example/cineapi/config/OpenApiConfig.java
+++ src/main/java/org/example/cineapi/config/OpenApiConfig.java
@@ -2,18 +2,36 @@ package org.example.cineapi.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import jakarta.validation.Valid;
+import org.jspecify.annotations.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI cineAPiOpenApi(){
+    public OpenAPI cineAPiOpenApi() {
         return new OpenAPI()
                 .info(new Info()
                         .title("CineAPI")
                         .description("API para gerencamento de filmes, diretores e avaliações")
-                        .version("1.0")                );
+                        .version("1.0"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(@NonNull @Valid CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5174")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
